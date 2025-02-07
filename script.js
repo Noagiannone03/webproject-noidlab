37d10396c53a4ce841026421e563322c931f2c71
Noa <noa.giannone@gmail.com>
2025-02-07 16:02:39 +0100

    version 3 aprem 7 fevrier

--- script.js
+++ script.js
@@ -369,4 +369,10 @@ window.onload = function() {
       console.error(error);
     });
   });
+
+
+
+  document.getElementById('backToTop').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
   
